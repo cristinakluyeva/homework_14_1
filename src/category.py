d2e0447abe4e1aa87d3e7e510e5d779fa103134b
Cristina <kristikavai@mail.ru>
2025-03-17 02:24:26 +0300

    add new methods to class: Category and Products

--- src/category.py
+++ src/category.py
@@ -1,3 +1,5 @@
+from src.product import Product
+
 class Category:
     """Класс представляет категории товаров"""
     name: str
@@ -9,6 +11,36 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name # Название категории
         self.description = description # Описание категории
-        self.products = products # список товаров, входящих в данную категорию
+        self.__products = products # список товаров, входящих в данную категорию
         Category.category_count += 1 # количество категорий
-        Category.products_count += len(self.products) # количество товаров по данной категории
+        Category.products_count += len(self.__products) # количество товаров по данной категории
+
+    @property
+    def products(self):
+        products_str = ''
+        for prod in self.__products:
+            products_str += f'{prod.name}, Цена: {prod.price}, Остаток: {prod.quantity}\n'
+        return products_str
+
+    @products.setter
+    def products(self, new_product: Product):
+        self.__products.append(new_product)
+        Category.products_count += 1
+
+    @property
+    def products_in_list(self):
+        return self.__products
+
+
+
+if __name__ == '__main__':
+    cat_1 = Category('Овощи', 'Растительные продукты', [Product('помидоры', 'красный, содержит много витамина С', 250.0, 13),
+    Product('огурцы', 'диетический продукт, содержит много воды', 149.99, 20),
+    Product('кабачки', 'сильное мочегонное средство', 325.7, 120),
+    Product('морковь', 'содержит много клетчатки и витамин К', 45.0, 37)])
+    print(cat_1.name, cat_1.description, Category.category_count)
+    print(cat_1.products)
+    #new_product = Product('свекла', 'Восстанавливает кровеснабжение', 50.0, 40)
+    cat_1.products = Product('свекла', 'Восстанавливает кровеснабжение', 50.0, 40)
+    print(cat_1.products)
+    print(Category.products_count, cat_1.products_in_list)
--- src/product.py
+++ src/product.py
@@ -4,9 +4,47 @@ class Product:
     description: str
     price: float
     quantity: int
+    product_class_list = []
 
     def __init__(self, name, description, price, quantity):
         self.name = name # Название товара/продукта
         self.description = description # Описание товара
-        self.price = price # Стоимость товара
+        self.__price = price # Стоимость товара
         self.quantity = quantity # Доступное количество товара(в наличие/ на складе)
+        Product.product_class_list.append({'name': self.name,
+                                           'description': self.description,
+                                           'price': self.__price,
+                                           'quantity': self.quantity})
+
+    @classmethod
+    def new_product(cls, add_product:dict):
+        for products_dict in cls.product_class_list:
+                if products_dict['name'] == add_product['name']:
+                    products_dict['quantity'] += add_product['quantity']
+                    if products_dict['price'] < add_product['price']:
+                        products_dict['price'] = add_product['price']
+                    return products_dict
+                else:
+                    name = add_product ['name']
+                    description = add_product['description']
+                    price = add_product['price']
+                    quantity = add_product['quantity']
+                    return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """Возвращает приватный атрибут класса"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        """Повышает или понижает стоимость товара новую цену на товар"""
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        elif 0 <new_price <= self.__price:
+            user_input = input('Если вы хотите понизить стоимость товара введите: yes')
+            if user_input.startswith(('Y','y','У', 'у')):
+                self.__price = new_price
+        elif new_price > self.price:
+            self.__price = new_price
+        return self.__price
